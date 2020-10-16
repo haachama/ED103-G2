81c21dd9e41c321628079b490c10fb25e85a5a4b
haachama <wood830919@gmail.com>
2020-10-16 17:23:24 +0800

    修改了泡泡SASS完整版，首頁進度到面鏡結束

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// parallax fish
+var scene = document.getElementById('scene');
+var parallax = new Parallax(scene);
+
+var fish02 = document.getElementsByClassName("fish02")[0];
+fish02.style.left = "30px"
+fish02.style.top = "130px"
+fish02.style.display = "inline-block"
+
+var fish03 = document.getElementsByClassName("fish03")[0];
+fish03.style.left = "350px"
+fish03.style.top = "100px"
+
+var fish04 = document.getElementsByClassName("fish04")[0];
+fish04.style.left = "150px"
+fish04.style.top = "190px"
+
+var fishNemo = document.getElementsByClassName("fishNemo")[0];
+fishNemo.style.left = "450px"
+fishNemo.style.top = "200px"
