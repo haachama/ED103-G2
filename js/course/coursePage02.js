6ea3339040161717d3415908b18530d315862a22
haachama <wood830919@gmail.com>
2020-10-20 20:57:24 +0800

    整合文采頁面 和 首頁

---
+++ js/course/coursePage02.js
@@ -0,0 +1,13 @@
+$(function(){
+    $("a.courseTab").on("click", function(e){
+      e.preventDefault();
+      
+      /* 將頁籤列表移除所有 -on，再將指定的加上 -on */
+      $(this).closest("ul").find("a.courseTab").removeClass("-on");
+      $(this).addClass("-on");
+      
+      /* 找到對應的頁籤內容，加上 -on 來顯示 */
+      $("div.courseTab").removeClass("-on");
+      $("div.courseTab." + $(this).attr("data-target")).addClass("-on");
+    });
+  });
